ac3849b7c07b6ced50dbba8bd076ef9631c77128
harjeevansingh <harjeevansingh01@gmail.com>
2019-10-18 10:00:29 +0530

    Studentb dashboard added

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,23 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
 from django.contrib import auth
+
+
+def signup(request):
+    if request.method == "POST":
+        if request.POST["password1"] == request.POST["password2"]:
+            try:
+                user = User.objects.get(username=request.POST["username"])
+                return render(request, 'accounts/signup.html', {'error': 'The username has been already taken.'})
+
+            except User.DoesNotExist:
+                user = User.objects.create_user(request.POST["username"], password=request.POST["password1"])
+                auth.login(request, user)
+                return redirect('home')
+        else:
+            return render(request, 'accounts/signup.html', {'error': 'The passwords do not match.'})
+    else:
+        return render(request, 'accounts/signup.html')
 
 
 def login(request):
@@ -16,3 +34,7 @@ def login(request):
         return render(request, 'accounts/login.html')
 
 
+def logout(request):
+    if request.method == 'POST':
+        auth.logout(request)
+        return redirect('home')
